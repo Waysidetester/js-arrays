6abe622af257a82c8b79efc06da7160c6cf2840a
WaysideTester <dylanmurray2119@gmail.com>
2018-08-28 18:45:35 -0500

    Completed mini challenge'

--- main.js
+++ main.js
@@ -4,3 +4,40 @@ const printDom = (stringToPrint, divID) => {
 };
 
 console.log('I am linked');
+
+const cats = ['fluffy', 'spike', 'killer'];
+
+console.log('First cat:',cats[0]);
+
+const favoriteAnimal = 'cow';
+
+const favoriteAnimalArray= favoriteAnimal.split('');
+
+console.log('fav animal array:', favoriteAnimalArray);
+
+const numArray = [1,2,3,4,5];
+
+const newNum = numArray.join('');
+
+console.log('numArray:',numArray);
+console.log('newNum:',newNum);
+
+//write func that takes a word and tells us if it is a palindrome
+
+const palChecker = (word) => {
+    //yes if palindrome
+    //no if not
+    let newWord = word.split('');
+    newWord = newWord.reverse();
+    newWord = newWord.join('');
+    if (newWord === word) {
+        console.log(`${word} is a Palindrome`);
+    } else {
+        console.log(`${word} is NOT a Palindrome`);
+    }
+
+};
+
+palChecker('mom');
+palChecker('monkeyButt');
+palChecker('racecar');
